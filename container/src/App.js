9c1b96df4b980498401403d20c87652b6bacc0cc
siddharth <siddharth.jain@peopleinteractive.in>
2022-02-24 01:34:06 +0530

    initial commit

---
+++ container/src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import MarketingApp from './components/MarketingApp';
+
+export default () => {
+  return <div>
+    Hi there!!
+    <hr />
+    <MarketingApp />
+  </div>
+}
